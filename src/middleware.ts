83034ca74027d9f1d60035ce54ae4d33a89b69c2
MARTIN SANCHEZ URREGO <martin.sanchez654@pascualbravo.edu.co>
2025-09-01 12:26:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button size="icon

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,8 @@ export function middleware(request: NextRequest) {
   const sessionCookie = request.cookies.get('secure-access-session');
   const { pathname } = request.nextUrl
 
-  // If the user is not logged in and tries to access a protected page
-  if (!sessionCookie && pathname !== '/login') {
+  // Allow access to login and manual pages without a session
+  if (!sessionCookie && pathname !== '/login' && pathname !== '/manual') {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
