839402bb51391bf5ce27c3ed3a97f7b5a49e4ebc
JUAN DAVID BENJUMEA RONDON <juan.benjumea316@pascualbravo.edu.co>
2025-09-14 20:04:03 +0000

    Mejora el campo de fecha y habilita para seleccionar mes y año y que no

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -15,6 +16,7 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
+  const currentYear = new Date().getFullYear();
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
@@ -23,7 +25,9 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex gap-2",
+        vhidden: "hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -53,6 +57,9 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      captionLayout="dropdown-buttons"
+      fromYear={currentYear - 100}
+      toYear={currentYear}
       components={{
         IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
